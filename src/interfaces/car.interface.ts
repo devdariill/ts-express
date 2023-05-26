01442f7b1de92a99cb88f6f5949a59da9aa0bf8c
nATOr <gowluonade@gmail.com>
2023-05-26 00:23:16 -0500

    t

--- src/interfaces/car.interface.ts
+++ src/interfaces/car.interface.ts
@@ -1,6 +1,10 @@
+export enum Gas {
+  Gasoline = 'gasoline',
+  Electric = 'electric',
+}
 export interface Car {
   color: string
-  gas: 'gasoline' | 'electric'
+  gas: Gas
   year: number
   description: string
   price: number
