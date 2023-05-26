f3760aee6f49f154ddc6586bdf40ecb985d698ea
nATOr <gowluonade@gmail.com>
2023-05-25 23:52:47 -0500

    controller

---
+++ src/controllers/items.controllers.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express'
+
+const getItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (error) {
+    console.log('🚀 ~ file: items.controllers.ts:7 ~ getItem ~ error:', error)
+  }
+}
+const getItems = (req: Request, res: Response) => {
+  try {
+
+  } catch (error) {
+    console.log('🚀 ~ file: items.controllers.ts:16 ~ getItems ~ error:', error)
+  }
+}
+const updateItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (error) {
+    console.log('🚀 ~ file: items.controllers.ts:24 ~ updateItem ~ error:', error)
+  }
+}
+const posetItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (error) {
+    console.log('🚀 ~ file: items.controllers.ts:32 ~ posetItem ~ error:', error)
+  }
+}
+const deleteItem = (req: Request, res: Response) => {
+  try {
+
+  } catch (error) {
+    console.log('🚀 ~ file: items.controllers.ts:40 ~ deleteItem ~ error:', error)
+  }
+}
+
+export { deleteItem, getItem, getItems, posetItem, updateItem }
+// eslint-disable-next-line no-multiple-empty-lines
+
