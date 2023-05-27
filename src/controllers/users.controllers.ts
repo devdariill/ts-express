44f7b074d41dac9915f197cbbe648ea14938ddf5
nATOr <gowluonade@gmail.com>
2023-05-27 02:33:37 -0500

    register-controller

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from 'express'
+import { registerUser } from '../services/auth.service'
+import { handleHttp } from '../utils/error.handle'
 
 // getUsar, postUser
-const loginCtrl = async (req: Request, res: Response) => {
+const loginCtrl = async ({ body }: Request, res: Response) => {
 }
-const registerCtrl = async (req: Request, res: Response) => {
+const registerCtrl = async ({ body }: Request, res: Response) => {
+  const { email, password, name, description } = body
+  const response = await registerUser({ email, password, name, description })
+  if (response == null) {
+    return handleHttp(res, 'USER_ALREADY_EXIST')
+  }
+  res.json({ response })
 }
 
 export { loginCtrl, registerCtrl }
