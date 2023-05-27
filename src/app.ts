4c12e3527a7c3656c0b8f5b57df4b605c3be88a6
nATOr <gowluonade@gmail.com>
2023-05-27 02:30:22 -0500

    _

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
 import express from 'express'
+import authsRoutes from './routes/auth.routes'
 import itemsRoutes from './routes/items.routes'
 
 import 'dotenv/config'
@@ -15,5 +16,6 @@ app.use(express.json())
 dbConnect().then(() => console.log('Database connected')).catch((e) => console.log('Database error', e))
 
 app.use('/items', itemsRoutes)
+app.use('/users', authsRoutes)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
